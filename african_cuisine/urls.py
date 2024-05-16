dfc6fec3fce7c79e28718e58ffa9d86edd82289a
Morgan Asare <ohenemorgan@gmail.com>
2024-05-16 22:18:08 +0000

    Add cuisine app to the project

--- african_cuisine/urls.py
+++ african_cuisine/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from cuisine.views import my_cuisine
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('cuisine/', my_cuisine, name='cuisine'),
 ]
