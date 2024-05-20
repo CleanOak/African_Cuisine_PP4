cac1476d7439524be22a9b557cf99af349fdac70
Morgan Asare <ohenemorgan@gmail.com>
2024-05-20 00:44:48 +0000

    add cuisine model

--- cuisine/models.py
+++ cuisine/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Cuisine(models.Model):
+    title = models.CharField(max_length=200,
+    unique=True)
+    slug = models.SlugField(max_length=200,
+    unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE,
+    related_name="recipe_posts")
+    description = models.TextField(blank=True)
+    instructions = models.TextField()
+    
+    def __str__(self):
+        return self.title
