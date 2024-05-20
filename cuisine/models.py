55652e2a77d1281d081b9e1f281df4fe29951aab
Morgan Asare <ohenemorgan@gmail.com>
2024-05-20 13:42:11 +0000

    add ingredient and comment models

--- cuisine/models.py
+++ cuisine/models.py
@@ -15,3 +15,33 @@ class Cuisine(models.Model):
     
     def __str__(self):
         return self.title
+
+
+class Ingredients(models.Model):
+    category = models.ForeignKey(Cuisine, on_delete=models.CASCADE,
+    related_name='recipe_category')
+    recipe = models.ForeignKey(Cuisine, on_delete=models.CASCADE,
+    related_name='ingredients')
+    name = models.CharField(max_length=100)
+    quantity = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f'{self.quantity} of {self.name} for {self.recipe.title}'
+
+
+class Comment(models.Model):
+    cuisine = models.ForeignKey(Cuisine, on_delete=models.CASCADE,
+    related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE,
+    related_name='commenter')
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+
+    class Meta:
+        ordering = ["-created_on"]
+
+    
+    def __str__(self):
+        return f'Comment {self.body}. by {self.author}'
--- cuisine/views.py
+++ cuisine/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import generic
+from .models import Cuisine
 
 # Create your views here.
-def my_cuisine(request):
-    return HttpResponse("Hello, Cuisine!")
+class CuisineList(generic.ListView):
+    
